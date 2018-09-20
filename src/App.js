8e276ec58c34744f72afb3577b8ddc18dd5ec28d
dalek3 <lina1318@gmail.com>
2018-09-20 16:27:30 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
 
 class App extends Component {
+  
+  state = {}
+  componentDidMount() {
+    setTimeout(() => {
+      this._getMovies();
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
+    })
+    return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.movies ? this._renderMovies() : 'loading...' }
       </div>
     );
   }
